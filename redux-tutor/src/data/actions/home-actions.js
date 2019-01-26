188cea60112142584d06d4af45565fb64cc02b1d
Cael Rowley <caelrowley@gmail.com>
2019-01-26 16:27:36 +0000

    Added web3 import

--- redux-tutor/src/data/actions/home-actions.js
+++ redux-tutor/src/data/actions/home-actions.js
@@ -1,9 +1,31 @@
+import web3 from '../../services/web3';
+import myToken from '../../services/my-token';
+
 const updateHomeText = (homeText) => ({
   type: 'CHANGE_HOME_TEXT',
   homeText
 })
 
 export const changeHomeText = () => async (dispatch, getState) => {
+  const [account] = await web3.eth.getAccounts();
+  console.log(account)
+
+
+
+  let balance = await myToken.methods.balanceOf(account).call({
+    from: account
+  })
+  console.log(balance)
+
+  // let transfer = await myToken.methods.transfer('0x7A5d089701E1baC31cBE9c15BDa39486d7D45e49', 1000).send({
+  //   from: account
+  // })
+
+  balance = await myToken.methods.balanceOf(account).call({
+    from: account
+  })
+  console.log(balance)
+
   const state = getState();
   dispatch(updateHomeText('this is the new text'));
 }
---
+++ redux-tutor/src/services/web3.js
@@ -0,0 +1,21 @@
+import Web3 from 'web3';
+
+let web3 = null
+
+if (window.ethereum) {
+  try {
+    window.ethereum.enable();
+    web3 = new Web3(window.ethereum);
+  } catch (error) {
+
+  }
+}
+else if (window.web3) {
+  web3 = new Web3(window.web3.currentProvider);
+}
+else {
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+  console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+}
+
+export default web3
